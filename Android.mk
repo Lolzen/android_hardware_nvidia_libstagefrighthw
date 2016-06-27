1667b47cb3586db31a8a5335b3cbb8299e27a011
Aaron Kling <webgeek1234@gmail.com>
2016-06-27 18:51:34 -0500

    Fix makefile to compile in an aosp compatible source tree

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-include $(NVIDIA_DEFAULTS)
+include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES :=
 LOCAL_SRC_FILES += omxplugin.cpp
@@ -10,7 +10,7 @@ LOCAL_C_INCLUDES += vendor/tv/frameworks/base/include
 endif
 
 LOCAL_C_INCLUDES += frameworks/native/include/media/hardware
-LOCAL_C_INCLUDES += $(TEGRA_TOP)/multimedia-partner/openmax/include/openmax/il
+LOCAL_C_INCLUDES += 3rdparty/nvidia/multimedia-includes/openmax/il
 LOCAL_SHARED_LIBRARIES := \
         libutils \
         libdl \
@@ -22,7 +22,5 @@ LOCAL_PRELINK_MODULE := false
 
 #TODO: Remove following lines before include statement and fix the source giving warnings/errors
 LOCAL_NVIDIA_NO_EXTRA_WARNINGS := 1
-include $(NVIDIA_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
+include $(BUILD_SHARED_LIBRARY)
 
