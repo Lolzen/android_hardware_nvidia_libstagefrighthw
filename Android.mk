dea37fefe7f4b397c380ff5f69df8cd724ff7ba1
Aaron Kling <webgeek1234@gmail.com>
2017-01-18 13:46:46 -0600

    Output to vendor and only build 32 bit

--- Android.mk
+++ Android.mk
@@ -5,7 +5,9 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := omxplugin.cpp
 LOCAL_C_INCLUDES += frameworks/native/include/media/hardware
 LOCAL_SHARED_LIBRARIES := libutils libdl liblog
+LOCAL_MODULE_PATH := $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE := libstagefrighthw
+LOCAL_MULTILIB := 32
 include $(BUILD_SHARED_LIBRARY)
 
 endif # TARGET_BOARD_PLATFORM == tegra
