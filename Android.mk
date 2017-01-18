a49861d3a205ba93336b7bdcfc94dc5a5a2b81b9
Aaron Kling <webgeek1234@gmail.com>
2017-01-18 14:21:56 -0600

    Use framework openmax includes

--- Android.mk
+++ Android.mk
@@ -4,6 +4,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := omxplugin.cpp
 LOCAL_C_INCLUDES += frameworks/native/include/media/hardware
+LOCAL_C_INCLUDES += frameworks/native/include/media/openmax
 LOCAL_SHARED_LIBRARIES := libutils libdl liblog
 LOCAL_MODULE_PATH := $($(TARGET_2ND_ARCH_VAR_PREFIX)TARGET_OUT_VENDOR_SHARED_LIBRARIES)
 LOCAL_MODULE := libstagefrighthw
